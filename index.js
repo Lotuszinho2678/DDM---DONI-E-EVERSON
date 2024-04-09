99d794e962913c788c905c876249acb9e3b7e9dd
Lotuszinho2678 <162191952+Lotuszinho2678@users.noreply.github.com>
2024-04-09 13:49:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const DATA = [
+    {
+      id: '1',
+      porcentagem: '45%',
+      titulo: 'Segunda-Feira',
+      imagem: 'https://img.freepik.com/vetores-premium/desenho-animado-joga-lixo-na-lixeira_29190-7167.jpg?w=740',
+      descricao: 'Jogo o lixo na rua',
+    },
+    {
+      id: '2',
+      porcentagem: '60%',
+      titulo: 'Terça-Feira',
+      imagem: 'URL_DA_IMAGEM',
+      descricao: 'Descrição para terça-feira',
+    },
+    {
+      id: '3',
+      porcentagem: '75%',
+      titulo: 'Quarta-Feira',
+      imagem: 'URL_DA_IMAGEM',
+      descricao: 'Descrição para quarta-feira',
+    },
+    {
+      id: '4',
+      porcentagem: '80%',
+      titulo: 'Quinta-Feira',
+      imagem: 'URL_DA_IMAGEM',
+      descricao: 'Descrição para quinta-feira',
+    },
+    {
+      id: '5',
+      porcentagem: '90%',
+      titulo: 'Sexta-Feira',
+      imagem: 'URL_DA_IMAGEM',
+      descricao: 'Descrição para sexta-feira',
+    },
+    {
+      id: '6',
+      porcentagem: '70%',
+      titulo: 'Sábado',
+      imagem: 'URL_DA_IMAGEM',
+      descricao: 'Descrição para sábado',
+    },
+    {
+      id: '7',
+      porcentagem: '55%',
+      titulo: 'Domingo',
+      imagem: 'URL_DA_IMAGEM',
+      descricao: 'Descrição para domingo',
+    },
+  ];
+  
+  export default DATA;
+  
